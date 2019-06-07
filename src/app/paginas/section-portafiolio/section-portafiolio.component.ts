c064cb5dfa31eb036afcc09f30b86fc94ffa99b5
usuario <admin@vangtry.com>
2019-06-07 11:24:27 -0500

    Template listo sin rutas

---
+++ src/app/paginas/section-portafiolio/section-portafiolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-portafiolio',
+  templateUrl: './section-portafiolio.component.html',
+  styleUrls: ['./section-portafiolio.component.scss']
+})
+export class SectionPortafiolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
